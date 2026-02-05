6fa3ce1bf2a8b407105a9901b129704d84d284b1
madewithbill <165756396+madewithbill@users.noreply.github.com>
2026-02-05 14:33:48 -0800

    adding hydration fallback and layout component for root route

--- app/root.tsx
+++ app/root.tsx
@@ -4,15 +4,18 @@ import {
   useSearchParams,
   Links,
   type URLSearchParamsInit,
+  ScrollRestoration,
 } from "react-router";
 import Navbar from "./components/Navbar";
 import {
   createContext,
   useState,
   type Dispatch,
+  type ReactNode,
   type SetStateAction,
 } from "react";
 import "./global.css";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 export type Context = {
   queue: string[];
@@ -28,6 +31,36 @@ const QueueContext = createContext<Context>({
   setSearchParams: () => {},
 });
 
+export function HydrateFallback() {
+  return (
+    <div className="w-full flex flex-col items-center gap-2 min-h-32">
+      <ArrowPathIcon className="block size-6 animate-spin" />
+      <h1 className="text-xl">Loading...</h1>
+    </div>
+  );
+}
+
+export function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/queueup-favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:image" content="/queueup-thumbnail.jpg" />
+        <Links />
+        <title>QueueUp Movie and TV Search</title>
+      </head>
+      <body className="px-4">
+        <Navbar />
+        {children}
+        <Scripts />
+        <ScrollRestoration />
+      </body>
+    </html>
+  );
+}
+
 export default function App() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [queue, setQueue] = useState(
@@ -39,27 +72,9 @@ export default function App() {
   );
 
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/queueup-favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:image" content="/queueup-thumbnail.jpg" />
-        <Links />
-        <title>queueup-movie-search</title>
-      </head>
-      <body>
-        <div className="px-4">
-          <Navbar />
-          <QueueContext
-            value={{ queue, setQueue, searchParams, setSearchParams }}
-          >
-            <Outlet />
-          </QueueContext>
-        </div>
-        <Scripts />
-      </body>
-    </html>
+    <QueueContext value={{ queue, setQueue, searchParams, setSearchParams }}>
+      <Outlet />
+    </QueueContext>
   );
 }
 
